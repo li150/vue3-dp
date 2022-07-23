83cb96c18f5252036f34c5e4f91c4e3fb5abe896
lyc <1449770614@qq.com>
2022-07-23 23:34:54 +0800

    feat: 初始化项目

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,10 @@
+<script lang="ts" setup>
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
 </template>
-
-<script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
-
-export default defineComponent({
-  name: "App",
-  components: {
-    HelloWorld,
-  },
-});
-</script>
 
 <style lang="less">
 #app {
