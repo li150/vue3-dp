9992a47c49d8045f3c70755824a62b9721b57a6c
li150 <1449770614@qq.com>
2022-08-01 23:39:47 +0800

    feat: 初始化tailwind.css配置文件

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+	purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"], //是对应的文件内容效果生效
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {}
+	},
+	variants: {
+		extend: {}
+	},
+	plugins: []
+}
